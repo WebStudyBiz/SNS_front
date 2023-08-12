ca5a97767c999b6adbee7530f534c0aa77a24735
jeehwan <tjtojan@naver.com>
2023-08-12 14:15:35 +0900

    feat: SearchBar 개발

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,7 @@ const StyledNavbar = styled.div`
   display: flex;
   justify-content: space-between;
   position: fixed;
-  color: black;
-  background-color: green;
+  background-color: #fff;
   height: 60px;
   top: 0;
   width: 100%;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { styled } from "styled-components";
+import { FaSistrix } from "react-icons/fa6";
+
+const StyledSearchBar = styled.div`
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  margin: auto 0;
+  padding: 2px 15px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 8px #ddd;
+  height: 30px;
+  width: 100%;
+`;
+
+const StyledSearchInput = styled.input`
+  border: none;
+  height: 100%;
+  width: 100%;
+  margin-left: 10px;
+  &:focus {
+    outline: none;
+  }
+`;
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <StyledSearchBar>
+      <FaSistrix />
+
+      <StyledSearchInput placeholder="Search Friend, Post or Video... " />
+    </StyledSearchBar>
+  );
 }
 
 export default SearchBar;
